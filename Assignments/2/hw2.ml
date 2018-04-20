1ddcdbdff8f9a78aa1c91bef353b08155aff0458
Jonathan Wei-Hong Chu <weihong@lnxsrv06.seas.ucla.edu>
2018-04-19 18:17:04 -0700

    convert grammar

--- Assignments/2/hw2.ml
+++ Assignments/2/hw2.ml
@@ -7,7 +7,7 @@ let rec unwrapped_convert gram1 gram2 x = match gram1 with
 	| [] -> gram2
 	| g1item :: g1rest -> match g1item with
 		| (nt, ts) -> if nt =  x 
-					  then unwrapped_convert g1rest (ts :: gram2) x
+					  then unwrapped_convert g1rest (gram2@[ts]) x
 					  else unwrapped_convert g1rest gram2 x
 
 let convert_grammar gram1 = match gram1 with
