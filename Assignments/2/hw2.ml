0ee1f8d6263b2ab14fb8834e802f225f099d48a8
Jonathan Wei-Hong Chu <weihong@lnxsrv06.seas.ucla.edu>
2018-04-20 00:40:44 -0700

    tryna merge

--- Assignments/2/hw2.ml
+++ Assignments/2/hw2.ml
@@ -13,44 +13,51 @@ let rec unwrapped_convert gram1 gram2 x = match gram1 with
 let convert_grammar gram1 = match gram1 with
 	| (ss, rules) -> (ss, unwrapped_convert rules [])
 
+
 (*2: parse_prefix*)
-(* Array appendation. This is faster than @ op since it does not copy every element *)
-let rec append_array rhs = function
-	| [] -> rhs
-	| h::t -> h::(append_array rhs t)
-in
+let parse ss prod  = 
+	(*write functions within parse_prefix, better encapsulation*)
+	(*recurse through rules for nonterminal*)
+	(*EQUIVALENT: let rec expand_nt prod nt rules = match rules with*)
+	let rec expand_nt prod nt = function
+		(*no more rules; haven't found a match. Return None*)
+		| [] -> (fun accept d frag -> None)
+		| ruleitem :: rulesrest -> (fun accept d frag ->
+			(*local variable function calls, more readable*)
+			let checkitem = match_item prod ruleitem
 
-let rec expand_nt prod nt = function
-	| [] -> (fun accept d frag -> None)
-	| rulesitem :: rulesrest -> (fun accept d frag -> 
-		let item_match = match_tm prod rulesrest rulesitem (*added rulesrest parameter*)
-		and rest_match = expand_nt prod nt rulesrest in
-
-		let matcher = item_match accept (append_array [(nt, rules_head)] derivation) frag in
-		match matcher with
-			| None -> rest_match accept d frag
-			| _ -> matcher
-	)
-
-let rec match_tm prod rules = function
-	| [] -> (fun accept d frag -> accept d frag)
-	| (T t) :: rules ->
-		(fun accept derivation -> function
-			| [] -> None
-			| fragitem :: fragrest -> 
-				let matcher = match_tm prod rules
-			in if fragitem = t then matcher accept d fragrest
-			   else None
-		)
-	| (N nt) :: rules_tail ->
-		(fun accept d frag ->
-			let tail_matcher = expand_nt prod nt (prod_func nt)
-			and new_acceptor = match_tm prod rules accept
-			in tail_matcher new_acceptor d frag)
+			in match checkitem accept (d @ [(nt, ruleitem)]) frag (*reverse order append?*) with
+				(*return whatever acceptor returns*)
+				| None -> expand_nt prod nt rulesrest accept d frag
+				(*continue*)
+				| _ -> checkitem accept (d @ [(nt, ruleitem)]) frag
 		)
 
-let parse_prefix gram = 
-	let ss = fst gram
-	and prod = snd gram in
+	(*check what the next item is and handle it*)
+	and match_item prod = function
+		(*no more rules; return what acceptor returned*)
+		| [] -> (fun accept d frag -> accept d frag)
+		(*item was a terminal symbol ==> check for match*)
+		| (T tm) :: rulesrest ->
+			(fun accept d -> function
+				| [] -> None
+				| fragitem :: fragrest ->
+					let matcher = match_item prod rulesrest in 
+					if fragitem = tm then 
+						(*Found a match! Now check fragrest*)
+						matcher accept d fragrest
+					else None (*terminal didn't match fragitem*)
+			)
+		(*item was a nonterminal ==> feed it back into expand_nt*)
+		| (N nt) :: rulesrest ->
+			(fun accept d frag ->
+				let checkrest = expand_nt prod nt (prod nt)
+				and new_acceptor = match_item prod rulesrest accept
+				in checkrest new_acceptor d frag
+			)
+	in
+	(*start by expanding the start symbol, program will recursively expand the rest*)
+	(* (prod ss) evaluates to the rules corresponding to start symbol*)
 	fun accept frag -> expand_nt prod ss (prod ss) accept [] frag
 
+let parse_prefix gram = parse (fst gram) (snd gram)
